12f32705f4ae9ca961529b4ae9977f453e903065
EndrisMoh <endrismd@gmail.com>
2022-12-14 11:25:54 -0800

    A function that goes through a binary tree using level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -103,7 +103,7 @@ void pop(levelorder_queue_t **head)
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
  * The value in the node must be passed as a parameter to this function.
- * If tree or func is NULL, do nothing
+ * If tree or func is NULL, do nothing.
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
