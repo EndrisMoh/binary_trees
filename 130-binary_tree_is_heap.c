19f0cc67f59d92e7ee5a635b2a1d8fc5e79695cd
EndrisMoh <endrismd@gmail.com>
2022-12-15 11:51:04 -0800

    A function that checks if a binary tree is a valid Max Binary Heap

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,49 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Function that measures the height of a binary tree
- * @tree: a pointer to the root node of the tree to check
- * Return: the height
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0, else return height.
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l = 0;
-	size_t r = 0;
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (tree->parent != NULL);
-		if (tree)
-		{
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		}
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((l > r) ? l : r);
-		}
+	}
+	return (0);
 }
 
 /**
- * binary_tree_balance - Measures balance factor of a binary tree
- * @tree: a pointer to the root node of the tree to check
- * Return: balanced factor
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the balance factor.
+ *
+ * Return: If tree is NULL, return 0, else return balance factor.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int right = 0, left = 0, total = 0;
-
 	if (tree)
-	{
-		left = ((int)binary_tree_height(tree->left));
-		right = ((int)binary_tree_height(tree->right));
-		total = left - right;
-	}
-	return (total);
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+
+	return (0);
 }
 
 /**
